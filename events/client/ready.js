820c8eb8ab8edf628ae74ef36957695778084017
mikebots <61569234+mikebots@users.noreply.github.com>
2020-06-02 01:08:47 -0600

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
-const Giveaway = require("../../database/model/Giveaway");
-const MessageModel = require("../../database/model/message");
-const database = require("../../database/database");
+//const Giveaway = require("../../database/model/Giveaway");
+/*const MessageModel = require("../../database/model/message");
+const database = require("../../database/database");*/
 
-const {scheduleGiveaways } = require("../../utils/structures/giveaway");
+/*const {scheduleGiveaways } = require("../../utils/structures/giveaway");*/
 module.exports=async(bot)=>{
    
-    const current = new Date();
+    /*const current = new Date();
     const giveaways = await Giveaway.find({
         endsOn: { $gt: current }
     });
     await scheduleGiveaways(bot, giveaways);
-    database.then(()=> console.log("Connected to MongoDB")).catch(err => console.log(err));
+    database.then(()=> console.log("Connected to MongoDB")).catch(err => console.log(err));*/
     
     bot.channels.cache.get('698780336439558205').send(`${bot.user.username} has restarted and is now online.`)
     bot.user.setActivity(`${bot.users.cache.size} users | ${process.env.PREFIX}help`, {type: "STREAMING"}, {url: "https://discord.gg/kugRv6Y"});
