d0abe9137dd87407b830365d29ee1f6173cfa696
mikebots <61569234+mikebots@users.noreply.github.com>
2020-06-06 17:42:12 -0600

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,7 +4,7 @@ module.exports=async(bot)=>{
    
    
    
-    bot.channels.cache.get('698780336439558205').send(`${bot.user.username} has restarted and is now online.`)
+   
     bot.user.setActivity(`${bot.users.cache.size} users | ${process.env.PREFIX}help`, {type: "STREAMING"});
     console.log(`${bot.user.tag} is online and has loaded\n${bot.commands.size} commands\nBot ping is ${bot.ws.ping}ms\nRun ${process.env.PREFIX}help for any command help`)
 }
